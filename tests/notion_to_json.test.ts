46d8f5d1bd4ed34d4e7258dc41ad0895c5008820
Henrique Fuschini <henriquefuschini@gmail.com>
2024-03-19 20:35:09 -0500

    add tests for unmapped and mixed payload cases

--- tests/notion_to_json.test.ts
+++ tests/notion_to_json.test.ts
@@ -283,7 +283,6 @@ describe("Notion to JSON", () => {
         });
     });
 
-    // TODO: Empty properties tests
     test("Empty Title", () => {
         baseNotionObject.properties = {
             Title: {
@@ -404,7 +403,175 @@ describe("Notion to JSON", () => {
         });
     });
 
-    // TODO: Mixed tests
+    // Mixed payload
+    test("Mixed properties", () => {
+        baseNotionObject.properties = {
+            Title: {
+                id: "title",
+                type: "title",
+                title: [
+                    {
+                        type: "text",
+                        text: {
+                            content: "page 1",
+                            link: null,
+                        },
+                        annotations: {
+                            bold: false,
+                            italic: false,
+                            strikethrough: false,
+                            underline: false,
+                            code: false,
+                            color: "default",
+                        },
+                        plain_text: "page 1",
+                        href: null,
+                    },
+                ],
+            },
+            Description: {
+                id: "HbZT",
+                type: "rich_text",
+                rich_text: [
+                    {
+                        type: "text",
+                        text: {
+                            content: "There is some ",
+                            link: null,
+                        },
+                        annotations: {
+                            bold: false,
+                            italic: false,
+                            strikethrough: false,
+                            underline: false,
+                            code: false,
+                            color: "default",
+                        },
+                        plain_text: "There is some ",
+                        href: null,
+                    },
+                    {
+                        type: "text",
+                        text: {
+                            content: "text",
+                            link: null,
+                        },
+                        annotations: {
+                            bold: true,
+                            italic: false,
+                            strikethrough: false,
+                            underline: false,
+                            code: false,
+                            color: "default",
+                        },
+                        plain_text: "text",
+                        href: null,
+                    },
+                    {
+                        type: "text",
+                        text: {
+                            content: " in this property!",
+                            link: null,
+                        },
+                        annotations: {
+                            bold: false,
+                            italic: false,
+                            strikethrough: false,
+                            underline: false,
+                            code: false,
+                            color: "default",
+                        },
+                        plain_text: " in this property!",
+                        href: null,
+                    },
+                ],
+            },
+            Number: {
+                id: "DT%3Eg",
+                type: "number",
+                number: 2,
+            },
+            Select: {
+                id: "FSau",
+                type: "select",
+                select: {
+                    id: "b5b79bb7-e513-4513-9c0f-8cf81392b189",
+                    name: "option 1",
+                    color: "red",
+                },
+            },
+            "Multi Select": {
+                id: "%3EP%60%7D",
+                type: "multi_select",
+                multi_select: [
+                    {
+                        id: "82303241-ae20-4690-bd0b-349a1528954c",
+                        name: "option 2",
+                        color: "red",
+                    },
+                    {
+                        id: "d521ee51-b4ad-468d-979a-d230875c1cf8",
+                        name: "option 1",
+                        color: "green",
+                    },
+                ],
+            },
+            Status: {
+                id: "ynbl",
+                type: "status",
+                status: {
+                    id: "13edf371-8bb1-4d19-921d-a095831dfab0",
+                    name: "Done",
+                    color: "green",
+                },
+            },
+            Date: {
+                id: "MJSZ",
+                type: "date",
+                date: {
+                    start: "2024-03-16T00:00:00.000-05:00",
+                    end: "2024-03-17T00:00:00.000-05:00",
+                    time_zone: null,
+                },
+            },
+            Checkbox: {
+                id: "jWlx",
+                type: "checkbox",
+                checkbox: true,
+            },
+            URL: {
+                id: "jWlx",
+                type: "url",
+                url: "https://www.notion.so",
+            },
+        };
+        expect(notionToJson(baseNotionObject)).toEqual({
+            Title: "page 1",
+            Description: "There is some text in this property!",
+            Number: 2,
+            Select: "option 1",
+            "Multi Select": ["option 2", "option 1"],
+            Status: "Done",
+            Date: {
+                start: "2024-03-16T00:00:00.000-05:00",
+                end: "2024-03-17T00:00:00.000-05:00",
+            },
+            Checkbox: true,
+            URL: "https://www.notion.so",
+        });
+    });
 
-    // TODO: unmapped types tests (valid and invalid)
+    // Unmapped type test
+    test("Unmapped type", () => {
+        baseNotionObject.properties = {
+            UnmappedType: {
+                id: "jWlx",
+                type: "unmapped_type",
+                unmapped_type: "unmapped_value",
+            },
+        };
+        expect(notionToJson(baseNotionObject)).toEqual({
+            UnmappedType: "unmapped_value",
+        });
+    });
 });
