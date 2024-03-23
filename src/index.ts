42ba6fb3a541d6a0954c50739502c1d05d912475
Henrique Fuschini <henriquefuschini@gmail.com>
2024-03-23 16:38:27 -0500

    add config for publishing package

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello via Bun!");
+import { jsonToNotion } from "./json_to_notion";
+import { notionToJson } from "./notion_to_json";
+
+exports.jsonToNotion = jsonToNotion;
+exports.notionToJson = notionToJson;
