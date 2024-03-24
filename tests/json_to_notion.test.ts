ae894a7a6b90c91ab74fbc78c525200165688d6a
Henrique Fuschini <henriquefuschini@gmail.com>
2024-03-23 19:47:18 -0500

    add mixed types test to json to notion flow

--- tests/json_to_notion.test.ts
+++ tests/json_to_notion.test.ts
@@ -330,4 +330,69 @@ describe("JSON to Notion", () => {
             });
         });
     });
+
+    // mixed types
+    test("Mixed types", () => {
+        const input = {
+            Title: "page 1",
+            Description: "There is some **text** in this property!",
+            Number: 2,
+            Select: "option 1",
+            "Multi Select": ["option 2", "option 1"],
+            Status: "Done",
+            Date: {
+                start: "2024-03-16T00:00:00.000-05:00",
+                end: "2024-03-17T00:00:00.000-05:00",
+            },
+            Checkbox: true,
+            URL: "https://www.notion.so",
+        };
+
+        expect(jsonToNotion(input, baseTableSchema)).toEqual({
+            Title: {
+                title: [
+                    {
+                        text: {
+                            content: "page 1",
+                        },
+                    },
+                ],
+            },
+            Number: {
+                number: 2,
+            },
+            Select: {
+                select: {
+                    name: "option 1",
+                },
+            },
+            "Multi Select": {
+                multi_select: [
+                    {
+                        name: "option 2",
+                    },
+                    {
+                        name: "option 1",
+                    },
+                ],
+            },
+            Status: {
+                status: {
+                    name: "Done",
+                },
+            },
+            Date: {
+                date: {
+                    start: "2024-03-16T00:00:00.000-05:00",
+                    end: "2024-03-17T00:00:00.000-05:00",
+                },
+            },
+            Checkbox: {
+                checkbox: true,
+            },
+            URL: {
+                url: "https://www.notion.so",
+            },
+        });
+    });
 });
